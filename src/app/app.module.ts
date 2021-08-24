99871849831bcb09fdf3c90d6c398ba466c955a5
peng_xiang <cxz0917001997@gmail.com>
2021-08-24 21:13:27 +0800

    add Onsen UI + Flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { OnsenModule } from 'ngx-onsenui';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DeviceInfoPageComponent } from './feature/page/device-info-page/device-info-page.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceInfoPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OnsenModule,
+    FlexLayoutModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
