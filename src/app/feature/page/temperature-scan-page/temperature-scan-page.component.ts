f212fe741342a5bda943b9a80619c95f977da361
彭浚翔 <cxz0917001997@gmail.com>
2021-10-06 14:48:51 +0800

    feat 新版辨識頁 - 流程實作

--- src/app/feature/page/temperature-scan-page/temperature-scan-page.component.ts
+++ src/app/feature/page/temperature-scan-page/temperature-scan-page.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject, Observable, timer, interval } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subject, Observable, timer, interval, of } from 'rxjs';
 import { WebcamImage } from 'ngx-webcam';
 import { tap, map, takeUntil, finalize } from 'rxjs/operators';
 import { fromEvent } from 'rxjs';
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./temperature-scan-page.component.scss']
 })
 export class TemperatureScanPageComponent implements OnInit {
+  @ViewChild('pageRef', { read: ElementRef }) pageRef: any;
   dialogSetting = {
     isShow: false,
     isSuccess: true,
@@ -22,51 +23,41 @@ export class TemperatureScanPageComponent implements OnInit {
   testFlag = true;
   deviceorientationObs$ = new Observable();
   buffer: any[] = [];
-  resList: any[] = [];
   endSubject = new Subject();
   isSuccess = false;
   isShowRefreshBtn = false;
+  isScanning = false;
+  temperature = 0;
+  warnTemperature = 37;
   constructor(
     private http: HttpClient,
-    private router: Router
-  ) {}
+    private router: Router,
+    private el: ElementRef
+  ) { }
+
   ngOnDestroy(): void {
     this.endSubject.next();
   }
   ngOnInit(): void {
-    this.listenDeviceorientation();
-    this.autoSnapshot();
   }
 
-  autoSnapshot() {
+  startScan() {
     const time = 6000;
     const delay = 300;
+    this.isScanning = true;
     interval(delay).pipe(
       finalize(() => {
+        this.isScanning = false;
         if (this.isSuccess) {
           return;
         }
-        // this.openResDialog(false, '辨識失敗 請重新整理').pipe(
-        //   tap(() => this.isShowRefreshBtn = true)
-        // ).subscribe()
+        this.openResDialog(false, '辨識失敗 請重新辨識').subscribe()
       }),
-      takeUntil(timer(time))
+      takeUntil(timer(time)),
+      takeUntil(this.endSubject)
     ).subscribe(() => {
       this.triggerSnapshot();
     });
-  }
-
-  listenDeviceorientation() {
-    this.deviceorientationObs$ = fromEvent(window, 'deviceorientation').pipe(
-      map((event: any) => {
-        const { alpha, beta, gamma } = event;
-        return { alpha, beta, gamma };
-      })
-    );
-  }
-
-  resetSnapshot() {
-    delete this.webcamImage;
   }
 
   triggerSnapshot(): void {
@@ -76,41 +67,42 @@ export class TemperatureScanPageComponent implements OnInit {
   handleImage(webcamImage: WebcamImage): void {
     this.webcamImage = webcamImage;
     this.buffer.push(webcamImage.imageAsBase64);
-    // const params = {
-    //   username: webcamImage.imageAsBase64
-    // };
-    // this.http.post('https://23b7-163-17-132-191.ap.ngrok.io/post_submit', params).pipe(
+    const params = {
+      username: webcamImage.imageAsBase64
+    };
+    // of({ temperature: 35.6 }).pipe(
     //   takeUntil(this.endSubject)
     // ).subscribe((res: any) => {
-    //   if (res === '0') {
-    //     return;
-    //   }
-    //   const { temperature } = res;
-    //   if (+temperature !== 0) {
-    //     this.isSuccess = true;
-    //     this.openResDialog(true, `體溫 ${temperature}`).pipe(
-    //       tap(() => {
-    //         this.endSubject.next();
-    //         this.router.navigate(['device-info']);
-    //       })
-    //     ).subscribe();
-    //   }
-    // });
+    this.http.post('https://23b7-163-17-132-191.ap.ngrok.io/post_submit', params).pipe(
+      takeUntil(this.endSubject)
+    ).subscribe((res: any) => {
+      if (res === '0') {
+        return;
+      }
+      const { temperature } = res;
+      if (+temperature !== 0) {
+        this.isSuccess = true;
+        this.temperature = +temperature;
+        this.endSubject.next();
+        this.openResDialog(true, `辨識成功 請按下送出`).pipe(
+          tap(() => {
+            this.pageToBottom();
+          })
+        ).subscribe();
+      }
+    });
+  }
+
+  pageToBottom() {
+    this.pageRef.nativeElement.scrollTop = 99999;
+  }
+
+  pageToTop() {
+    this.pageRef.nativeElement.scrollTop = 0;
   }
 
   get triggerObservable(): Observable<void> {
     return this.trigger.asObservable();
-  }
-
-  uploadImg() {
-    this.isUploading = true;
-    timer(3000).subscribe(() => {
-      this.isUploading = false;
-      this.testFlag
-        ? this.openResDialog(true, '體溫 38.6').subscribe()
-        : this.openResDialog(false, '失敗').subscribe();
-      this.testFlag = !this.testFlag;
-    });
   }
 
   openResDialog(isSuccess: boolean, msg: string) {
@@ -126,7 +118,9 @@ export class TemperatureScanPageComponent implements OnInit {
     );
   }
 
-  refresh(): void {
-    window.location.reload();
+  reset() {
+    this.isSuccess = false;
+    this.temperature = 0;
+    timer(100).subscribe(() => this.pageToTop());
   }
 }
