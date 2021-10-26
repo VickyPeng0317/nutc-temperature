07330a6e94a178ff97fcbdf063825aeb45f3db5e
peng_xiang <cxz0917001997@gmail.com>
2021-10-26 21:05:58 +0800

    modify env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  isMock: true,
+  isMock: false,
   production: true,
   scanApiPath: 'https://23b7-163-17-132-191.ap.ngrok.io/post_submit',
   nutcApi: 'https://dsl2501_70.nutc.edu.tw:5053/NutcTempApi'
