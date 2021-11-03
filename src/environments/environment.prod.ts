22c949658afc0e52fcd5e36cc48a7871ac179c76
peng_xiang <cxz0917001997@gmail.com>
2021-11-03 22:08:54 +0800

    modify api path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   isMock: false,
   production: true,
-  scanApiPath: 'https://23b7-163-17-132-191.ap.ngrok.io/post_submit',
-  nutcApi: 'https://dsl2501_70.nutc.edu.tw:5053/NutcTempApi',
+  scanApiPath: 'http://163.17.136.70:5000/post_submit',
+  nutcApi: 'https://dsl250170.nutc.edu.tw:5054/NutcTempApi',
   HOT: 37.5,
   COOL: 35
 };
