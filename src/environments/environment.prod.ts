b6a0d120aeb8530c3467375c344398f2349d63d0
peng_xiang <cxz0917001997@gmail.com>
2021-11-05 23:09:26 +0800

    modify scan api path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   isMock: false,
   production: true,
-  scanApiPath: 'http://163.17.136.70:5000/post_submit',
+  scanApiPath: 'https://dsl250170.nutc.edu.tw:5001/post_submit',
   nutcApi: 'https://dsl250170.nutc.edu.tw:5054/NutcTempApi',
   HOT: 37.5,
   COOL: 35
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   isMock: true,
   production: false,
-  scanApiPath: 'http://163.17.136.70:5000/post_submit',
+  scanApiPath: 'https://dsl250170.nutc.edu.tw:5001/post_submit',
   nutcApi: 'https://dsl250170.nutc.edu.tw:5054/NutcTempApi',
   HOT: 37.5,
   COOL: 35
